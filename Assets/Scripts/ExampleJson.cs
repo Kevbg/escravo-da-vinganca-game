8e1707da7e37ea7d96c6f1b99824a81e8c560395
Bruno Geroto <nevesilva@gmail.com>
2016-03-09 05:32:37 -0300

    example json

---
+++ Assets/Scripts/ExampleJson.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using LitJson;
+
+
+public class ExampleJson : MonoBehaviour {
+	private string jsonString;
+
+	// Use this for initialization
+	void Start () {
+		jsonString = File.ReadAllText (Application.dataPath + "/Json/String.json");
+
+		Debug.Log (jsonString);
+
+		JsonData jsonObject = JsonMapper.ToObject(jsonString);
+
+		Debug.Log (jsonObject ["menu"] ["text_1"] ["portuguese"]);
+		Debug.Log (jsonObject ["level_1"] ["text_1"] ["english"]);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
